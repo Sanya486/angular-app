186c60e6d346892d5660d1853cfd2cc04fdaff93
Sanya486 <sanyok486@gmail.com>
2023-12-28 21:03:13 +0200

    fix styles, add new route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { FetchingDataComponent } from './pages/fetching-data/fetching-data.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
     title: 'Home page',
-    }
+  },
+  {
+    path: 'fetchhing-data',
+    component: FetchingDataComponent,
+    title: 'Fetching',
+  },
 ];
---
+++ src/app/pages/fetching-data/fetching-data.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fetching-data',
+  standalone: true,
+  imports: [],
+  templateUrl: './fetching-data.component.html',
+  styleUrl: './fetching-data.component.scss'
+})
+export class FetchingDataComponent {
+
+}
